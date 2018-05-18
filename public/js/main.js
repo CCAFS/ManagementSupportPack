6eb8c72035f477fc5ebb55e07f636ec12b0736f5
Kenji Tanaka <K.TANAKA@cgiar.org>
2018-05-18 16:20:25 -0500

    forms without validation, data captured locally

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,89 @@
+var filterType,  listSelected,
+themePath = './vendor/',
+guideSelected;
+
 $(document).ready(function() {
 
   //Attach Events
   attachEvents();
 
+  //Click Events
+  ValidateFields();
+
   // Set countdown
   $('.countdown-styled').countdown({
-      date: "May 25, 2018 15:03:26",
-      render: function(data) {
-        var el = $(this.el);
-        el.empty()
-          .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
-          .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
-          .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
-          .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
-      }
+    date: "May 25, 2018 15:03:26",
+    render: function(data) {
+      var el = $(this.el);
+      el.empty()
+      .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
+      .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
+      .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
+      .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+    }
   });
 
-});
+
+
+
+  /**********************Click Events********************************************/
+    // Step 3 (Terms and conditions) email contact
+    $( "a.download.1" ).click(function() {
+      filterType = $(this).attr("id"); 
+      console.log(filterType); 
+      $("[name='check-guidelines']:checked")
+
+      updateGuideSelected("check-guidelines");   
+      $("#step3").show().siblings().hide();
+    });
+
+    // Step 4 (Terms and conditions) form contact
+    $( "a.download.2" ).click(function() {
+      $("#step4").show().siblings().hide();
+    });
+
+    // Step 5 (Links for download)
+    $( "a.download.3" ).click(function() {
+
+      $("#step5").show().siblings().hide();
+      printGuidelinesToDownload();
+      $('.loading').fadeOut();    
+    });
+
+    $( "a.download.5" ).click(function() {
+      guideSelected = new Array();
+      guideSelected[0] = {
+        id: 999,
+        name: "Data Management Support [Full package]",
+        type: 3,
+        source: "http://www.reading.ac.uk/ssc/resources/ccafs_data_management_support_pack.pdf",
+        importance_level: "Optional"
+      }
+      $("#step3").show().siblings().hide();
+    });
+
+  });
 
 /*********************************** Events ***********************************/
 
 function attachEvents(){
+
   // Select an option Event
   $('li.selectionComponent:not(current)').on('click', selectAnOption );
+}
+
+/*****************************************************************************/
+
+function ValidateFields(){
+
+  function validateEmail(emailField) {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    if(emailField == "" || !emailReg.test(emailField)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
 
 /********************************** Functions *********************************/
@@ -55,9 +116,9 @@ function selectAnOption(){
     }
     // Find recomended
     $.ajax({
-       url: './api/guidelinesLevels/'+options.r+'/'+options.s+'/'+options.c,
-       beforeSend: function(){
-         $('.loading').fadeIn();
+     url: './api/guidelinesLevels/'+options.r+'/'+options.s+'/'+options.c,
+     beforeSend: function(){
+       $('.loading').fadeIn();
          // Clear data
          $('.results-query ul').empty();
        },
@@ -74,9 +135,11 @@ function selectAnOption(){
          //Add guidelines
          $.each(guidelines, function(i, guideline){
            // Create guideline from template
-           var $guideline = $('#guideline-template').clone(true).removeAttr('id');
+           var $guideline = $('#guideline-template').clone(true);
+
            // Add guideline title
            $guideline.find('.title').text(guideline.code+' '+guideline.name);
+
            // Add importance level and the style
            $guideline.find('.level').text(guideline.importance_level);
            $guideline.find('.level').addClass(guideline.importance_level);
@@ -92,44 +155,85 @@ function selectAnOption(){
        complete: function(data) {
          $('.loading').fadeOut();
        }
-    });
+     });
   }
+
+
 }
 
 function showResultsBlock(blockName){
   $('.resultsBlock .results-'+blockName).slideDown().siblings().slideUp();
 }
 
-/*********************************** Utils ************************************/
 
-function getClassParameter(selector,cssName) {
-  var check = cssName + "-";
-  var className = $(selector).attr('class') || '';
-  var type = $.map(className.split(' '), function(val,i) {
-    if(val.indexOf(check) > -1) {
-      return val.slice(check.length, val.length);
+
+function printGuidelinesToDownload(){
+  var content = '<ul>';
+  
+  downloadText = ' Download',
+  className = '',
+
+
+  downloadLink = themePath+'download.php?file=';
+
+  content += "<li>";
+  content += "    <a class='downloadLink "+className+"' href='"+downloadLink+"' >";
+  content += "    <span class='download' style='float:right'><img src='"+themePath+"images/dl.png'>"+downloadText+"</span></a>";
+  content += "</li>";
+  
+  content += '</ul>';
+  $( "#step5 #guidelines" ).html(content);
+
+}
+
+    /* This event is when the Checkbox was Selected or Unselected
+    and fill a array guideSelected with new list selected      */
+    function updateGuideSelected(name){
+      $("input[name^='"+name+"']").change(function() {
+        guideSelected = new Array();
+        $("input[name^='"+name+"']:checked").each(function(i) {
+          guideSelected[i] = dataLoaded[$(this).attr('id')];
+        });
+                console.log(guideSelected);
+      });
     }
-  });
-  return((type.join(' ')) || 'none');
-}
 
-function cutText(str, l){
-  if (str.length > l){
-    str = str.slice(0,l) + '...';
-  }
-  return str;
-}
 
-jQuery.fn.classParam = function(cssName) {
-  return getClassParameter(this, cssName)
-};
 
-$.fn.extend({
-    animateCss: function (animationName) {
+    /*********************************** Utils ************************************/
+
+
+
+    function getClassParameter(selector,cssName) {
+      var check = cssName + "-";
+      var className = $(selector).attr('class') || '';
+      var type = $.map(className.split(' '), function(val,i) {
+        if(val.indexOf(check) > -1) {
+          return val.slice(check.length, val.length);
+        }
+      });
+      return((type.join(' ')) || 'none');
+    }
+
+    function cutText(str, l){
+      if (str.length > l){
+        str = str.slice(0,l) + '...';
+      }
+      return str;
+    }
+
+    jQuery.fn.classParam = function(cssName) {
+      return getClassParameter(this, cssName)
+    };
+
+    $.fn.extend({
+      animateCss: function (animationName) {
         var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
         this.addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
+          $(this).removeClass('animated ' + animationName);
         });
         return this;
-    }
-});
+      }
+    });
+
+
---
+++ src/routes/api/person.php
@@ -0,0 +1,47 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+//$app = new \Slim\App;
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            //->withHeader('Content-Type', 'application/json');
+});
+
+// Get All Customers
+$app->get('/api/guidelinesLevels/{role}/{stage}/{category}', function(Request $request, Response $response){
+    $r = $request->getAttribute('role');
+    $s= $request->getAttribute('stage');
+    $c= $request->getAttribute('category');
+    $sql = "SELECT g.id,g.code,g.name,g.type,g.source, il.importance_level
+            FROM msp_importance_levels il
+            INNER JOIN msp_categories c ON il.category_id = c.id
+            INNER JOIN msp_stages s ON il.stage_id = s.id
+            INNER JOIN msp_roles r ON il.role_id = r.id
+            INNER JOIN msp_guidelines g ON il.guideline_id = g.id
+            WHERE il.role_id = ".$r." and il.stage_id= ".$s." and il.category_id=".$c."
+            ORDER BY g.code";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($customers);
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
